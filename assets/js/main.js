e45377bb278f41897d4b9c33595db5b704f529f2
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2022-11-21 17:07:31 -0300

    Currículo 1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,7 +106,7 @@ let resumeButton = document.getElementById('resume-button');
 // Html2pdf options
 let opt = {
   margin:       0,
-  filename:     'myResume.pdf',
+  filename:     'Ronan Vieira do Carmo Junior.pdf',
   image:        { type: 'jpeg', quality: 1 },
   html2canvas:  { scale: 4, logging: true },
   jsPDF:        { format: 'a4', orientation: 'portrait' }
